4244f75f821e000bb257b0eb6a525afb338ea04e
Ravikumar Jha <jharavi153@gmail.com>
2024-11-14 15:42:03 +0530

    Date-time_Datatype

---
+++ 1.Datatypes.js
@@ -0,0 +1,9 @@
+//Date & Time Object & Their Method 
+
+const now = new Date()
+console.log(now)                   // 2024-11-14T10:00:59.906Z
+console.log(now.getFullYear())         //2024
+console.log(now.getMonth())        //10
+console.log(now.getDay())          //4
+console.log(now.getHours());
+console.log(now.getSeconds());
