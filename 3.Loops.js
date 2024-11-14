6c24e65476a738092eddaea4f268d74b6d21c2b8
Ravikumar Jha <jharavi153@gmail.com>
2024-11-14 23:05:26 +0530

    Implementation of Array

--- 3.Loops.js
+++ 3.Loops.js
@@ -1,88 +1,93 @@
-// In programming languages to carry out repetitive task we use different kinds of loops.
-// Here are the types of loops in JavaScript:
+// // In programming languages to carry out repetitive task we use different kinds of loops.
+// // Here are the types of loops in JavaScript:
 
-// 1.for loop
-// 2.while loop
-// 3.do...while loop
-// 4.for...in loop
-// 5.for...of loop
-// 6.forEach() method (specific to arrays)
+// // 1.for loop
+// // 2.while loop
+// // 3.do...while loop
+// // 4.for...in loop
+// // 5.for...of loop
+// // 6.forEach() method (specific to arrays)
 
-for(let i = 1; i<=6; i++){
-    console.log(i)
-  }
+// for(let i = 1; i<=6; i++){
+//     console.log(i)
+//   }
 
-const numbers = [1, 2, 3, 4, 5]
-let sum = 0
-for(let i = 0; i < numbers.length; i++){
-  sum  = sum + numbers[i]  // can be shorten, sum += numbers[i]
-}
+// const numbers = [1, 2, 3, 4, 5]
+// let sum = 0
+// for(let i = 0; i < numbers.length; i++){
+//   sum  = sum + numbers[i]  // can be shorten, sum += numbers[i]
+// }
 
-console.log(sum)  // 15
+// console.log(sum)  // 15
 
-// while Loop
+// // while Loop
 
-let i = 0
-while (i <= 5) {
-  console.log(i)      //0,1,2,3,4,5
-  i++
-}
-
-
-// do while loop 
-
-let j = 0
-do {
-  console.log(j)
-  j++
-} while (j <5)             // 0,1,2,3,4
+// let i = 0
+// while (i <= 5) {
+//   console.log(i)      //0,1,2,3,4,5
+//   i++
+// }
 
 
-// for of Loop    
-// for (const element of arr)
+// // do while loop 
 
-const webTechs = [
-  'HTML',
-  'CSS',
-  'JavaScript',
-  'React',
-  'Redux',
-  'Node',
-  'MongoDB'
-]
-
-for (const tech of webTechs) {
-  console.log(tech.toUpperCase())         //It is very hand way to iterate through an array if we are not interested in the index of each element in the array.
-}
+// let j = 0
+// do {
+//   console.log(j)
+//   j++
+// } while (j <5)             // 0,1,2,3,4
 
 
-// For in loop :-This loop is typically used to iterate over the indices or properties of an object.
+// // for of Loop    
+// // for (const element of arr)
 
-// For each Lop :-This is an array method that executes a given function on each element of the array. It's more functional in style and doesn't require an explicit loop.
+// const webTechs = [
+//   'HTML',
+//   'CSS',
+//   'JavaScript',
+//   'React',
+//   'Redux',
+//   'Node',
+//   'MongoDB'
+// ]
 
-const arr = [10, 20, 30];
-arr.forEach((value, index) => {
-  console.log(index, value);           // Logs 0 10, 1 20, 2 30
-});
+// for (const tech of webTechs) {
+//   console.log(tech.toUpperCase())         //It is very hand way to iterate through an array if we are not interested in the index of each element in the array.
+// }
+
+
+// // For in loop :-This loop is typically used to iterate over the indices or properties of an object.
+
+// // For each Lop :-This is an array method that executes a given function on each element of the array. It's more functional in style and doesn't require an explicit loop.
+
+// const arr = [10, 20, 30];
+// arr.forEach((value, index) => {
+//   console.log(index, value);           // Logs 0 10, 1 20, 2 30
+// });
  
 
-// Break Keyward :- Break is used to interrupt a loop.
-for(let i = 0; i <= 5; i++){
-  if(i == 3){
-    break                //0,1,2
-  }
-  console.log(i)
+// // Break Keyward :- Break is used to interrupt a loop.
+// for(let i = 0; i <= 5; i++){
+//   if(i == 3){
+//     break                //0,1,2
+//   }
+//   console.log(i)
+// }
+
+
+// // Continue :- We use the keyword continue to skip a certain iterations.
+
+// for(let i = 0; i <= 5; i++){
+//   if(i == 3){
+//     continue            //0,1,2,4,5
+//   }
+//   console.log(i);
+// }
+
+
+for(let i =1 ; i<=100; i++)
+if(i%2!==0){
+  console.log(`${i}`);
 }
-
-
-// Continue :- We use the keyword continue to skip a certain iterations.
-
-for(let i = 0; i <= 5; i++){
-  if(i == 3){
-    continue            //0,1,2,4,5
-  }
-  console.log(i)
-}
-
 
 
---
+++ 4.Array.js
@@ -0,0 +1,76 @@
+// Array :- an Array can store multiple values. Each value in an array has an index, and each index has a reference in a memory address. 
+
+// Create a Array 
+const ars = new Array();
+console.log(ars);
+
+// using Bracket 
+
+const arrr = []
+console.log(arrr);
+
+const numbers = [0, 3.14, 9.81, 37, 98.6, 100] // array of numbers
+const fruits = ['banana', 'orange', 'mango', 'lemon'] // array of strings, fruits
+
+console.log('Numbers:', numbers)
+console.log('Number of numbers:', numbers.length)
+
+console.log(numbers.slice(1,5));   //[ 3.14, 9.81, 37, 98.6 ] cut out 1 & 5 index element 
+
+console.log('Fruits:', fruits)
+console.log('Number of fruits:', fruits.length)
+
+const rev = numbers.concat(fruits)     //Concatenating array using concat
+console.log(rev);
+console.log(rev.indexOf('mango'));     // to check element is present in array or not
+
+console.log(rev.includes('lemon'));          //To check if an item exist in an array. If it exist it returns the true else it returns false.
+ console.log(rev.toString());              // Converts array to string
+
+ console.log(rev.join('$ '));                 //Join: It is used to join the elements of the array, the argument we passed in the join method will be joined in the array and return as a string.
+
+// arr containing different data types
+const arr = [
+    'Asabeneh',
+    250,
+    true,
+    { country: 'Finland', city: 'Helsinki' },
+    { skills: ['HTML', 'CSS', 'JS', 'React', 'Python'] }
+] 
+
+console.log(arr)
+console.log(arr[2]);     // accessing array using its indexes
+let lastIndex = arr.length - 1;
+console.log(arr[lastIndex])
+
+console.log(typeof(arr));   // check datatype of variable
+
+arr[2]=false;                 //change the array element using index
+console.log(arr)
+
+const four4values = Array(6).fill(4)   // it creates 4 element values filled with '4'
+console.log(four4values)               //     [4, 4, 4, 4, 4, 4] 
+
+
+const array = [1, 2, 3, 4, 5];
+const splicedArray = array.splice(1, 2, 6, 7);        // removes [2, 3] and inserts [6, 7]
+console.log(splicedArray);                             // [2, 3] (elements removed)
+console.log(array);                             //     [1,6,7,4,5 ]
+
+array.push(8);                                 // Push: adding item in the end. 
+console.log(array);         
+
+array.pop();                                // Pop : remove last elemnt in array
+console.log(array);
+
+array.shift();                         //shift: Removing one array element in the beginning of the array.
+console.log(array);
+
+array.unshift(24);                  //unshift: Adding array element in the beginning of the array.
+console.log(array);                        
+
+array.reverse();                        //reverse: reverse the order of an array.
+console.log(array)
+
+array.sort((a, b) => a - b);              // Sorting numbers requires a compare function,
+console.log(array);
