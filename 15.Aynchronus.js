28f1b897e2362a5f067297d2b721113bd5753d0c
Ravikumar Jha <jharavi153@gmail.com>
2024-11-30 21:41:02 +0530

    callback hell

--- 15.Aynchronus.js
+++ 15.Aynchronus.js
@@ -33,4 +33,46 @@ const promise2 = new Promise((resolve) => setTimeout(resolve, 50, "Second"));
 Promise.race([promise1, promise2]).then((result) => console.log(result)); // Logs: Second
 
 
+// Simulating a sequence of asynchronous operations with nested callbacks
+function task1(callback) {
+  setTimeout(() => {
+    console.log("Task 1 completed");
+    callback();
+  }, 1000);
+}
+
+function task2(callback) {
+  setTimeout(() => {
+    console.log("Task 2 completed");
+    callback();
+  }, 1000);
+}
+
+function task3(callback) {
+  setTimeout(() => {
+    console.log("Task 3 completed");
+    callback();
+  }, 1000);
+}
+
+function task4(callback) {
+  setTimeout(() => {
+    console.log("Task 4 completed");
+    callback();
+  }, 1000);
+}
+
+// Nested callbacks leading to callback hell
+task1(() => {
+  task2(() => {
+    task3(() => {
+      task4(() => {
+        console.log("All tasks completed");
+      });
+    });
+  });
+});
+
+
+
 
