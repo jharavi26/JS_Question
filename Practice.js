a7b3f26d2c9599b9dcbee764196804f045651342
Ravikumar Jha <jharavi153@gmail.com>
2025-01-18 18:09:57 +0530

    Add Notes

--- Practice.js
+++ Practice.js
@@ -1,17 +1,8 @@
-const mypromise = new Promise((resolve, reject)=>{
-  let art = true;
-  setTimeout(()=>{
-  if(art){
-    resolve("Art is True")
-  }
-    else {
-      reject ("Art is False")
+const p1 = new Promise((resolve, reject)=> setTimeout(resolve, 1000, "art1"))
+const p2 = new Promise((resolve, reject)=> setTimeout(resolve, 2000, "art2"))
+const p3 = new Promise((resolve, reject)=> setTimeout(reject, 100, "art3"))
+const p4 = new Promise((resolve, reject)=> setTimeout(resolve, 3000, "art4"))
 
-    }
-  },3000)
-})
-
-
-mypromise.then(result=>console.log(result))
-.catch(error=>console.log("Error:" , error))
-
+Promise.race([p1,p2,p3,p4])
+  .then((res)=>console.log(res))
+  .catch((error)=>console.log(error))
