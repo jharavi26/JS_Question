2ece49a2eecaa32941c78ce44e5d8ef1147abf63
Ravikumar Jha <jharavi153@gmail.com>
2025-01-20 20:15:31 +0530

    practice

--- Practice.js
+++ Practice.js
@@ -1,1 +1,10 @@
 
+ function getData(){
+  fetch("https://dummyjson.com/products")
+  .then(res => res.json())
+  .then(result => {
+    document.getElementById("output1").innerHTML = result.products
+    .map(product => `<li>${product.id}: ${product.title}</li>`)
+    .join(""); 
+})
+}
